b61c79db4ce93fcdf4c5eeb621ebd1c3d42a0fdf
Cayque Martins <123041280+CayqueKah13@users.noreply.github.com>
2023-07-04 22:17:26 -0300

    COMPLETE WEBSITE

---
+++ pix_mail/messages.php
@@ -0,0 +1,13 @@
+<?php
+	$lang = array(
+        // English is the default language, you can change the langugae from the config.php file
+        'EN'=> array(
+            'check_email'=>'Por favor digite um email válido!',
+            'captcha'=>'Por favor faça a validação!',
+            'php_error'=>'Não foi possível enviar seu e-mail! Entre em contato com o administrator do site!', //CHECAR PHP
+            'success'=>'Olá, já recebemos sua mensagem. Em breve entraremos em contato',
+            'subscription'=>'Obrigado por se inscrever',
+            'email_exists'=>'E-mail já cadastrado',
+        )
+    );
+?>
